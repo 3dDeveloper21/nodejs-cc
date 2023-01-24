ebb160d62360e02787887bf52f666226dd8b09d3
3dDeveloper21 <3ddeveloper21@gmail.com>
2023-01-23 23:44:29 -0500

    local server home and about pages working

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  // set Header content type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+
+  // send about.html to the browser
+  //   fs.readFile("./views/about.html", (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //       res.end();
+  //     } else {
+  //       res.write(data);
+  //       res.end();
+  //     }
+  //   });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening to requests on port 3000");
+});
